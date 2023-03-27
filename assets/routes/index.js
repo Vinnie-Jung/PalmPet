7dc8b81470730bb1101bd9ccd8e3fc08b1e3a367
Vinícius Jung <viniciusjung@outlook.com>
2023-03-27 16:05:57 -0300

    Adicionado arquivo de rotas

---
+++ assets/routes/index.js
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Welcome from '../views/Welcome';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name = 'Welcome'
+                component = {Welcome}
+                options = {{ headerShown: false }}
+            />
+        </Stack.Navigator>
+    );
+};
