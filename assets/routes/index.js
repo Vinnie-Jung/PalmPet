22081b2eceb23b03d939072ac3a23b610a715d78
Vinícius Jung <viniciusjung@outlook.com>
2023-03-29 22:27:13 -0300

    Criada uma nova rota para a página de signUp

--- assets/routes/index.js
+++ assets/routes/index.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Welcome from '../views/Welcome';
 import LogIn from '../views/LogIn';
+import SignUp from '../views/SignUp';
 
 /*
  *  +-----------------------------------+
@@ -22,6 +23,12 @@ export default function Routes() {
             <Stack.Screen
                 name = 'LogIn'
                 component = {LogIn}
+                options = {{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name = 'SignUp'
+                component = {SignUp}
                 options = {{ headerShown: false }}
             />
         </Stack.Navigator>
