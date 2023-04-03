6a79f410d2c729735254effc9b401dd185e978e1
Vinícius Jung <viniciusjung@outlook.com>
2023-04-03 03:47:27 -0300

    Adicionados os arquivos para a página de Details (vazios)

---
+++ assets/views/Details/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../../styles/details';
+import { View } from 'react-native-animatable';
+
+export default function Details() {
+    <View>
+        <Text>Details</Text>
+        <Text>Details</Text>
+        <Text>Details</Text>
+        <Text>Details</Text>
+        <Text>Details</Text>
+    </View>
+}
